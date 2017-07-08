5fbc43b297a7d809e16ea4e9da81900d340c593c
zanoooda <zanooda@ya.ru>
2017-07-08 03:17:15 +0300

    Refactoring

--- js/paradux.js
+++ js/paradux.js
@@ -1,3 +1,6 @@
+// TODO: Make some better indication
+alert("Blue first!")
+
 var l = Math.floor((window.innerHeight >= window.innerWidth ? window.innerWidth : window.innerHeight) * 0.9);
 
 var container = document.getElementById("container");
@@ -9,47 +12,24 @@ var canvas = document.getElementById("paradux");
 canvas.width = l;
 canvas.height = l;
 
-// 
-canvas.addEventListener('mousemove', function (e) {
-    var rect = canvas.getBoundingClientRect();
-    var point = {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-    };
-    console.log(point.x + ", " + point.y);
-}, false);
-
-canvas.addEventListener('click', function (e) {
-    var rect = canvas.getBoundingClientRect();
-    var point = {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-    };
-    alert(point.x + ", " + point.y);
-}, false);
-
-canvas.addEventListener('touchstart', function (e) {
-    var rect = canvas.getBoundingClientRect();
-    var point = {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-    };
-    // Nan, NaN!
-    alert(point.x + ", " + point.y);
-}, false);
-
 var context = canvas.getContext("2d");
+
+// Variables
 
 // TODO: Save last movie or better all the history of the movies
 //       After it you will be able to create undo/redo button
 // TODO: Change hardcoded object to object with constructor
 var state = [[{ x: 0, y: 0, color: 1 }, { x: 0, y: 1, color: 2 }, { x: 0, y: 2, color: 1 }, { x: 0, y: 3, color: 2 }],
-[{ x: 1, y: 0, color: 2 }, { x: 1, y: 1, color: 0 }, { x: 1, y: 2, color: 0 }, { x: 1, y: 3, color: 0 }, { x: 1, y: 4, color: 1 }],
-[{ x: 2, y: 0, color: 1 }, { x: 2, y: 1, color: 0 }, { x: 2, y: 2, color: 0 }, { x: 2, y: 3, color: 0 }, { x: 2, y: 4, color: 0 }, { x: 2, y: 5, color: 2 }],
-[{ x: 3, y: 0, color: 2 }, { x: 3, y: 1, color: 0 }, { x: 3, y: 2, color: 1 }, { x: 3, y: 3, color: 0 }, { x: 3, y: 4, color: 2 }, { x: 3, y: 5, color: 0 }, { x: 3, y: 6, color: 1 }],
-[{ x: 4, y: 0, color: 1 }, { x: 4, y: 1, color: 0 }, { x: 4, y: 2, color: 0 }, { x: 4, y: 3, color: 0 }, { x: 4, y: 4, color: 0 }, { x: 4, y: 5, color: 2 }],
-[{ x: 5, y: 0, color: 2 }, { x: 5, y: 1, color: 0 }, { x: 5, y: 2, color: 0 }, { x: 5, y: 3, color: 0 }, { x: 5, y: 4, color: 1 }],
-[{ x: 6, y: 0, color: 1 }, { x: 6, y: 1, color: 2 }, { x: 6, y: 2, color: 1 }, { x: 6, y: 3, color: 2 }]];
+             [{ x: 1, y: 0, color: 2 }, { x: 1, y: 1, color: 0 }, { x: 1, y: 2, color: 0 }, { x: 1, y: 3, color: 0 }, { x: 1, y: 4, color: 1 }],
+             [{ x: 2, y: 0, color: 1 }, { x: 2, y: 1, color: 0 }, { x: 2, y: 2, color: 0 }, { x: 2, y: 3, color: 0 }, { x: 2, y: 4, color: 0 }, { x: 2, y: 5, color: 2 }],
+             [{ x: 3, y: 0, color: 2 }, { x: 3, y: 1, color: 0 }, { x: 3, y: 2, color: 1 }, { x: 3, y: 3, color: 0 }, { x: 3, y: 4, color: 2 }, { x: 3, y: 5, color: 0 }, { x: 3, y: 6, color: 1 }],
+             [{ x: 4, y: 0, color: 1 }, { x: 4, y: 1, color: 0 }, { x: 4, y: 2, color: 0 }, { x: 4, y: 3, color: 0 }, { x: 4, y: 4, color: 0 }, { x: 4, y: 5, color: 2 }],
+             [{ x: 5, y: 0, color: 2 }, { x: 5, y: 1, color: 0 }, { x: 5, y: 2, color: 0 }, { x: 5, y: 3, color: 0 }, { x: 5, y: 4, color: 1 }],
+             [{ x: 6, y: 0, color: 1 }, { x: 6, y: 1, color: 2 }, { x: 6, y: 2, color: 1 }, { x: 6, y: 3, color: 2 }]];
+
+var pairs = null;
+
+// Functions
 
 function render(state) {
     // TODO: Add options to highlight circles
@@ -90,5 +70,51 @@ function render(state) {
     }
 }
 
+// function getPairs(state) {
+
+    
+
+//     return null;
+// }
+
 // Here a magic starts
 render(state);
+
+//var pairs = getPairs(state);
+    // foreach pairs
+    // make line between two circles
+    // get center point of the line
+    // make circle around this point
+    // TODO: Make var for circles to detect collision
+    // save circle to an array
+
+// May be replace listeners
+canvas.addEventListener('mousemove', function (e) {
+    var rect = canvas.getBoundingClientRect();
+    var point = {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+    console.log(point.x + ", " + point.y);
+}, false);
+
+canvas.addEventListener('click', function (e) {
+    var rect = canvas.getBoundingClientRect();
+    var point = {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+
+    alert(point.x + ", " + point.y);
+
+}, false);
+
+canvas.addEventListener('touchstart', function (e) {
+    var rect = canvas.getBoundingClientRect();
+    var point = {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+    // Nan, NaN!
+    alert(point.x + ", " + point.y);
+}, false);
