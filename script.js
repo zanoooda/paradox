5bdfe3e4810d0f0ed0500fc8076a69ab9ded91a4
zanooda <zanooda@ya.ru>
2020-08-15 08:10:09 +0300

    bautify

--- script.js
+++ script.js
@@ -15,18 +15,15 @@ class Grid {
             radiusIndex++, radiusCellsQuantity = radiusIndex * 6) {
             for (let radiusCellsIndex = 0, directionIndex = 0;
                 radiusCellsIndex < radiusCellsQuantity;
-                radiusCellsIndex++,
-                radiusCellsIndex != 1 && (radiusCellsIndex - 1) % radiusIndex == 0 ? directionIndex++ : directionIndex
-            ) {
-                let direction = this.directions[directionIndex];
-                console.log(`radiusIndex: ${radiusIndex}, radiusCellsIndex: ${radiusCellsIndex}, directionIndex: ${directionIndex}, direction: {${direction.x}, ${direction.y}, ${direction.z}}`);
+                radiusCellsIndex++, radiusCellsIndex != 1 && (radiusCellsIndex - 1) % radiusIndex == 0 ? directionIndex++ : directionIndex) {
+                const direction = this.directions[directionIndex];
                 if (radiusCellsIndex == 0) {
                     this.cells.push(new Cell(radiusIndex * this.initialDirection.x, radiusIndex * this.initialDirection.y, radiusIndex * this.initialDirection.z));
-                    console.log(`${radiusIndex * this.initialDirection.x}, ${radiusIndex * this.initialDirection.y}, ${radiusIndex * this.initialDirection.z}`);
+                    console.log(`[ ${radiusIndex * this.initialDirection.x}, ${radiusIndex * this.initialDirection.y}, ${radiusIndex * this.initialDirection.z} ] radiusIndex: ${radiusIndex}, radiusCellsIndex: ${radiusCellsIndex}, directionIndex: ${directionIndex}`);
                 } else {
                     let previousCell = this.cells[this.cells.length - 1];
                     this.cells.push(new Cell(previousCell.x + direction.x, previousCell.y + direction.y, previousCell.z + direction.z));
-                    console.log(`${previousCell.x + direction.x}, ${previousCell.y + direction.y}, ${previousCell.z + direction.z}`);
+                    console.log(`[ ${previousCell.x + direction.x}, ${previousCell.y + direction.y}, ${previousCell.z + direction.z} ] radiusIndex: ${radiusIndex}, radiusCellsIndex: ${radiusCellsIndex}, directionIndex: ${directionIndex}`);
                 }
             }
         }
