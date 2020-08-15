36105a1080d226c0fd0902fbc0ca38a686ffecef
zanooda <zanooda@ya.ru>
2020-08-15 21:22:00 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-//TODO: Implement scalarSum() and scalarMultiplication()
 //TODO: Wrap second for into the func
 
 class Grid {
     constructor(radius = 4) {
-        this.cells = new Array();
-        for (let radiusIndex = 0, diameter = 1;
-            radiusIndex < radius;
-            radiusIndex++, diameter = radiusIndex * 6) {
+        this.cells = this.createGrid(radius);
+        // for (let radius = 0; radius < r; radius++) {
+        //     Vector.getDiameter
+        //     let diameter = radius == 0 ? 1 : radius * 6;
+        //     for (let diameterIndex = 0, directionIndex = 0;
+        //         diameterIndex < diameter;
+        //         diameterIndex++, diameterIndex != 1 && (diameterIndex - 1) % radius == 0 ? directionIndex++ : directionIndex) {
+        //         let vector = diameterIndex == 0 ? 
+        //             Vector.scalarMult(Vector.directions[4], radius) : 
+        //             Vector.add(this.cells[this.cells.length - 1], Vector.directions[directionIndex]);
+        //         this.cells.push(vector);
+        //         console.log(`{ ${vector.x}, ${vector.y}, ${vector.z} } radius: ${radius}, diameterIndex: ${diameterIndex}, directionIndex: ${directionIndex}`);
+        //     }
+        // }
+    }
+    createGrid(r) {
+        let cells = new Array();
+        for (let radius = 0; radius < r; radius++) {
+            Vector.getDiameter
+            let diameter = radius == 0 ? 1 : radius * 6;
             for (let diameterIndex = 0, directionIndex = 0;
                 diameterIndex < diameter;
-                diameterIndex++, diameterIndex != 1 && (diameterIndex - 1) % radiusIndex == 0 ? directionIndex++ : directionIndex) {
-                if (diameterIndex == 0) {
-                    this.cells.push(Vector.scalarMultiplication(Vector.directions[4], radiusIndex));
-                    console.log(`[ ${Vector.scalarMultiplication(Vector.directions[4], radiusIndex).x}, ${Vector.scalarMultiplication(Vector.directions[4], radiusIndex).y}, ${Vector.scalarMultiplication(Vector.directions[4], radiusIndex).z} ] radiusIndex: ${radiusIndex}, radiusCellsIndex: ${diameterIndex}, directionIndex: ${directionIndex}`);
-                } else {
-                    this.cells.push(Vector.addition(this.cells[this.cells.length - 1], Vector.directions[directionIndex]));
-                    console.log(`[ ${Vector.addition(this.cells[this.cells.length - 1], Vector.directions[directionIndex]).x}, ${Vector.addition(this.cells[this.cells.length - 1], Vector.directions[directionIndex]).y}, ${Vector.addition(this.cells[this.cells.length - 1], Vector.directions[directionIndex]).z} ] radiusIndex: ${radiusIndex}, radiusCellsIndex: ${diameterIndex}, directionIndex: ${directionIndex}`);
-                }
+                diameterIndex++, diameterIndex != 1 && (diameterIndex - 1) % radius == 0 ? directionIndex++ : directionIndex) {
+                let vector = diameterIndex == 0 ? 
+                    Vector.scalarMult(Vector.directions[4], radius) : 
+                    Vector.add(cells[cells.length - 1], Vector.directions[directionIndex]);
+                cells.push(vector);
+                console.log(`{ ${vector.x}, ${vector.y}, ${vector.z} } radius: ${radius}, diameterIndex: ${diameterIndex}, directionIndex: ${directionIndex}`);
             }
         }
+        return cells;
     }
 }
 class Vector {
@@ -35,15 +49,15 @@ class Vector {
         new Vector(1, -1, 0), // →
         new Vector(0, -1, 1)  // ↘
     ];
-    static scalarMultiplication(vector, scalar) {
+    static scalarMult(vector, scalar) {
         return new Vector(vector.x * scalar, vector.y * scalar, vector.z * scalar);
     }
-    static addition(vector1, vector2) {
+    static add(vector1, vector2) {
         return new Vector(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z);
     }
 }
-class Cell extends Vector {
-    constructor(...props) {
-        super(...props);
+class Cell {
+    constructor(vector) {
+        this.vector = vector;
     }
 }
