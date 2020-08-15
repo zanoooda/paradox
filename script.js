9bb5c421a551cfe253cd89fc3096ea3e2fa23328
zanooda <zanooda@ya.ru>
2020-08-15 22:22:23 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-//TODO: Wrap second for into the func
-
 class Grid {
     constructor(radius = 4) {
-        this.cells = this.createGrid(radius);
-        // for (let radius = 0; radius < r; radius++) {
-        //     Vector.getDiameter
-        //     let diameter = radius == 0 ? 1 : radius * 6;
-        //     for (let diameterIndex = 0, directionIndex = 0;
-        //         diameterIndex < diameter;
-        //         diameterIndex++, diameterIndex != 1 && (diameterIndex - 1) % radius == 0 ? directionIndex++ : directionIndex) {
-        //         let vector = diameterIndex == 0 ? 
-        //             Vector.scalarMult(Vector.directions[4], radius) : 
-        //             Vector.add(this.cells[this.cells.length - 1], Vector.directions[directionIndex]);
-        //         this.cells.push(vector);
-        //         console.log(`{ ${vector.x}, ${vector.y}, ${vector.z} } radius: ${radius}, diameterIndex: ${diameterIndex}, directionIndex: ${directionIndex}`);
-        //     }
-        // }
-    }
-    createGrid(r) {
-        let cells = new Array();
-        for (let radius = 0; radius < r; radius++) {
-            Vector.getDiameter
-            let diameter = radius == 0 ? 1 : radius * 6;
-            for (let diameterIndex = 0, directionIndex = 0;
-                diameterIndex < diameter;
-                diameterIndex++, diameterIndex != 1 && (diameterIndex - 1) % radius == 0 ? directionIndex++ : directionIndex) {
-                let vector = diameterIndex == 0 ? 
-                    Vector.scalarMult(Vector.directions[4], radius) : 
-                    Vector.add(cells[cells.length - 1], Vector.directions[directionIndex]);
-                cells.push(vector);
-                console.log(`{ ${vector.x}, ${vector.y}, ${vector.z} } radius: ${radius}, diameterIndex: ${diameterIndex}, directionIndex: ${directionIndex}`);
-            }
+        this.cells = new Array();
+        for (let r = 0; r < radius; r++) {
+            this.cells.push(Vector.getDiameter(r));
         }
-        return cells;
     }
 }
 class Vector {
@@ -49,11 +20,26 @@ class Vector {
         new Vector(1, -1, 0), // →
         new Vector(0, -1, 1)  // ↘
     ];
+    static startDirection = Vector.directions[4];
     static scalarMult(vector, scalar) {
         return new Vector(vector.x * scalar, vector.y * scalar, vector.z * scalar);
     }
     static add(vector1, vector2) {
         return new Vector(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z);
+    }
+    static getDiameter(radius) {
+        let diameter = new Array();
+        let diameterLength = radius == 0 ? 1 : radius * 6;
+        for (let diameterIndex = 0, directionIndex = 0;
+            diameterIndex < diameterLength;
+            diameterIndex++, diameterIndex != 1 && (diameterIndex - 1) % radius == 0 ? directionIndex++ : directionIndex) {
+            let vector = diameterIndex == 0 ? 
+                Vector.scalarMult(Vector.startDirection, radius) : 
+                Vector.add(diameter[diameter.length - 1], Vector.directions[directionIndex]);
+            diameter.push(vector);
+            console.log(`{ ${vector.x}, ${vector.y}, ${vector.z} } radius: ${radius}, diameterIndex: ${diameterIndex}, directionIndex: ${directionIndex}`);
+        }
+        return diameter;
     }
 }
 class Cell {
