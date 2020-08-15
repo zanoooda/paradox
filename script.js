9e5141b91401dd2c7c226db60279aa2cf4755fe8
zanooda <zanooda@ya.ru>
2020-08-16 00:30:29 +0300

    simplify

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
-class Grid {
-    constructor(radius = 4) {
-        this.cells = new Array();
-        for (let r = 0; r < radius; r++) {
-            this.cells.push(...Vector.getDiameter(r).map(vector => new Cell(vector)));
-        }
-    }
+const directions = [
+    { x: -1, y: 0, z: 1 }, // ↙
+    { x: -1, y: 1, z: 0 }, // ←
+    { x: 0, y: 1, z: -1 }, // ↖
+    { x: 1, y: 0, z: -1 }, // ↗
+    { x: 1, y: -1, z: 0 }, // →
+    { x: 0, y: -1, z: 1 }  // ↘
+]
+const startDirection = directions[4];
+function scalarMult(vector, scalar) {
+    return { x: vector.x * scalar, y: vector.y * scalar, z: vector.z * scalar };
 }
-class Cell {
-    constructor(vector) {
-        this.vector = vector;
-    }
+function add(vector1, vector2) {
+    return { x: vector1.x + vector2.x, y: vector1.y + vector2.y, z: vector1.z + vector2.z };
 }
-class Vector {
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+function getDiameter(radius) {
+    let diameter = [scalarMult(startDirection, radius)];
+    for (let diameterIndex = 1, directionIndex = 0; diameterIndex < radius * 6; directionIndex += diameterIndex++ % radius == 0) {
+        diameter.push(add(diameter[diameter.length - 1], directions[directionIndex]));
     }
-    static directions = [
-        new Vector(-1, 0, 1), // ↙
-        new Vector(-1, 1, 0), // ←
-        new Vector(0, 1, -1), // ↖
-        new Vector(1, 0, -1), // ↗
-        new Vector(1, -1, 0), // →
-        new Vector(0, -1, 1)  // ↘
-    ];
-    static startDirection = Vector.directions[4];
-    static scalarMult(vector, scalar) {
-        return new Vector(vector.x * scalar, vector.y * scalar, vector.z * scalar);
-    }
-    static add(vector1, vector2) {
-        return new Vector(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z);
-    }
-    static getDiameter(radius) {
-        let diameter = [Vector.scalarMult(Vector.startDirection, radius)];
-        for (let diameterIndex = 1, directionIndex = 0; diameterIndex < radius * 6; directionIndex += diameterIndex++ % radius == 0) {
-            diameter.push(Vector.add(diameter[diameter.length - 1], Vector.directions[directionIndex]));
-        }
-        return diameter;
-    }
+    return diameter;
 }
+
+let grid = [];
+for (let radius = 0; radius < 4; radius++) {
+    grid.push(...getDiameter(radius).map(vector => new Object({ cordinates: vector })));
+}
+grid.forEach(cell => {
+    console.log(JSON.stringify(cell))
+});
