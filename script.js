b59cbecf1830a65d78019f51094bef587e9dadfa
zanooda <zanooda@ya.ru>
2020-08-15 23:43:03 +0300

    refactoring

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ class Grid {
     constructor(radius = 4) {
         this.cells = new Array();
         for (let r = 0; r < radius; r++) {
-            this.cells.push(Vector.getDiameter(r));
+            this.cells.push(...Vector.getDiameter(r).map(vector => new Cell(vector)));
         }
+    }
+}
+class Cell {
+    constructor(vector) {
+        this.vector = vector;
     }
 }
 class Vector {
@@ -28,22 +33,10 @@ class Vector {
         return new Vector(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z);
     }
     static getDiameter(radius) {
-        let diameter = new Array();
-        let diameterLength = radius == 0 ? 1 : radius * 6;
-        for (let diameterIndex = 0, directionIndex = 0;
-            diameterIndex < diameterLength;
-            diameterIndex++, diameterIndex != 1 && (diameterIndex - 1) % radius == 0 ? directionIndex++ : directionIndex) {
-            let vector = diameterIndex == 0 ? 
-                Vector.scalarMult(Vector.startDirection, radius) : 
-                Vector.add(diameter[diameter.length - 1], Vector.directions[directionIndex]);
-            diameter.push(vector);
-            console.log(`{ ${vector.x}, ${vector.y}, ${vector.z} } radius: ${radius}, diameterIndex: ${diameterIndex}, directionIndex: ${directionIndex}`);
+        let diameter = [Vector.scalarMult(Vector.startDirection, radius)];
+        for (let diameterIndex = 1, directionIndex = 0; diameterIndex < radius * 6; directionIndex += (++diameterIndex - 1) % radius == 0) {
+            diameter.push(Vector.add(diameter[diameter.length - 1], Vector.directions[directionIndex]));
         }
         return diameter;
-    }
-}
-class Cell {
-    constructor(vector) {
-        this.vector = vector;
     }
 }
