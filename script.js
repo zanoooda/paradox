bd20517be2674dcf4f15b6d644e96a776d7df7e5
zanooda <zanooda@ya.ru>
2020-08-15 07:04:56 +0300

    refactor

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ class Grid {
             new Vector(1, -1, 0), // →
             new Vector(0, -1, 1)  // ↘  
         ];
-        this.firstDirection = this.directions[4];
+        this.initialDirection = this.directions[4];
         for (let radiusIndex = 0, radiusCellsQuantity = 1;
             radiusIndex < radius;
             radiusIndex++, radiusCellsQuantity = radiusIndex * 6) {
             for (let radiusCellsIndex = 0, directionIndex = 0;
                 radiusCellsIndex < radiusCellsQuantity;
-                radiusCellsIndex++, radiusCellsIndex != 1 && radiusCellsIndex % radiusIndex == 0 ? directionIndex++ : directionIndex) { //!
+                radiusCellsIndex++, //...
+                radiusCellsIndex != 1 && radiusCellsIndex % radiusIndex == 0 ? directionIndex++ : directionIndex) { //!
                 //radiusCellsIndex++, radiusCellsIndex % radiusIndex == 0 ? directionIndex++ : directionIndex) {
                 let direction = this.directions[directionIndex];
                 console.log(`radiusIndex: ${radiusIndex}, radiusCellsIndex: ${radiusCellsIndex}, directionIndex: ${directionIndex}, direction: {${direction.x}, ${direction.y}, ${direction.z}}`);
                 if (radiusCellsIndex == 0) {
-                    this.cells.push(new Cell(radiusIndex * this.firstDirection.x, radiusIndex * this.firstDirection.y, radiusIndex * this.firstDirection.z));
-                    console.log(`${radiusIndex * this.firstDirection.x}, ${radiusIndex * this.firstDirection.y}, ${radiusIndex * this.firstDirection.z}`);
+                    this.cells.push(new Cell(radiusIndex * this.initialDirection.x, radiusIndex * this.initialDirection.y, radiusIndex * this.initialDirection.z));
+                    console.log(`${radiusIndex * this.initialDirection.x}, ${radiusIndex * this.initialDirection.y}, ${radiusIndex * this.initialDirection.z}`);
                 } else {
                     let previousCell = this.cells[this.cells.length - 1];
                     this.cells.push(new Cell(previousCell.x + direction.x, previousCell.y + direction.y, previousCell.z + direction.z));
