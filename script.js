c161c590df179c4aef23e7134c724b81e00969ee
zanooda <zanooda@ya.ru>
2020-08-15 07:00:53 +0300

    edit directions and constructor

--- script.js
+++ script.js
@@ -2,24 +2,26 @@ class Grid {
     constructor(radius = 4) {
         this.cells = new Array();
         this.directions = [
-            new Vector(1, -1, 0), // →
             new Vector(-1, 0, 1), // ↙
             new Vector(-1, 1, 0), // ←
             new Vector(0, 1, -1), // ↖
             new Vector(1, 0, -1), // ↗
+            new Vector(1, -1, 0), // →
             new Vector(0, -1, 1)  // ↘  
         ];
+        this.firstDirection = this.directions[4];
         for (let radiusIndex = 0, radiusCellsQuantity = 1;
             radiusIndex < radius;
             radiusIndex++, radiusCellsQuantity = radiusIndex * 6) {
             for (let radiusCellsIndex = 0, directionIndex = 0;
                 radiusCellsIndex < radiusCellsQuantity;
-                radiusCellsIndex++, radiusCellsIndex % radiusIndex == 0 ? directionIndex++ : directionIndex) {
+                radiusCellsIndex++, radiusCellsIndex != 1 && radiusCellsIndex % radiusIndex == 0 ? directionIndex++ : directionIndex) { //!
+                //radiusCellsIndex++, radiusCellsIndex % radiusIndex == 0 ? directionIndex++ : directionIndex) {
                 let direction = this.directions[directionIndex];
-                console.log(`directionIndex: ${directionIndex}, direction: {${direction.x}, ${direction.y}, ${direction.z}}`);
+                console.log(`radiusIndex: ${radiusIndex}, radiusCellsIndex: ${radiusCellsIndex}, directionIndex: ${directionIndex}, direction: {${direction.x}, ${direction.y}, ${direction.z}}`);
                 if (radiusCellsIndex == 0) {
-                    this.cells.push(new Cell(radiusIndex * direction.x, radiusIndex * direction.y, radiusIndex * direction.z));
-                    console.log(`${radiusIndex * direction.x}, ${radiusIndex * direction.y}, ${radiusIndex * direction.z}`);
+                    this.cells.push(new Cell(radiusIndex * this.firstDirection.x, radiusIndex * this.firstDirection.y, radiusIndex * this.firstDirection.z));
+                    console.log(`${radiusIndex * this.firstDirection.x}, ${radiusIndex * this.firstDirection.y}, ${radiusIndex * this.firstDirection.z}`);
                 } else {
                     let previousCell = this.cells[this.cells.length - 1];
                     this.cells.push(new Cell(previousCell.x + direction.x, previousCell.y + direction.y, previousCell.z + direction.z));
