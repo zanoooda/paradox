1fd8e4c407f0def506ac30f2dc67fc289e143ad2
zanooda <zanooda@ya.ru>
2020-08-16 04:05:34 +0300

    refactoring

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const directions = [
     [1, 0, -1], // ↗
     [1, -1, 0], // →
     [0, -1, 1]  // ↘
-];
-const startDirection = directions[4];
+], startDirection = directions[4];
 function add(arr1, arr2) {
     return arr1.map((n, i) => n + arr2[i]);
 }
@@ -23,8 +22,32 @@ function getDiameter(radius) {
 function createGrid(radius) {
     let grid = [];
     for (let r = 0; r < radius; r++) {
-        grid.push(...getDiameter(r).map(c => new Object({ coord: c})));
+        grid.push(...getDiameter(r).map(c => new Object({ coord: c })));
     }
     return grid;
 }
-createGrid(4).forEach(c => console.log(JSON.stringify(c)));
+
+function getPoint(c) {
+    console.log(JSON.stringify(c));
+    //...
+    return [canvas.width / 2, canvas.height / 2];
+}
+function renderCell(cell) {
+    ctx.beginPath();
+    ctx.arc(...getPoint(cell.coord), cellRadius, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+function renderGrid(grid) {
+    grid.forEach(cell => {
+        renderCell(cell);
+    });
+}
+const radius = Math.min(document.body.getBoundingClientRect().width, document.body.getBoundingClientRect().height),
+    cellRadius = radius / 32,
+    canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+canvas.width = radius;
+canvas.height = radius;
+document.body.prepend(canvas);
+
+renderGrid(createGrid(4));
