f6a185979dc3f69eef19424d47c2ae8987cab8b2
zanooda <zanooda@ya.ru>
2020-08-16 10:44:48 +0300

    split  back into two files

---
+++ js/canvas.js
@@ -0,0 +1,25 @@
+const radius = Math.min(document.body.getBoundingClientRect().width, document.body.getBoundingClientRect().height),
+    cellRadius = radius / 32,
+    canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d');
+function getPoint(c) {
+    console.log(JSON.stringify(c));
+    //...
+    return [canvas.width / 2, canvas.height / 2];
+}
+function renderCell(cell) {
+    ctx.beginPath();
+    ctx.arc(...getPoint(cell.coord), cellRadius, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+function renderGrid(grid) {
+    grid.forEach(cell => {
+        renderCell(cell);
+    });
+}
+canvas.width = radius;
+canvas.height = radius;
+document.body.prepend(canvas);
+
+let grid = createGrid(4);
+renderGrid(grid);
