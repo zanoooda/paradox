34745a7b0ce8b5c9824ee603faf74ff19239aa6b
zanooda <zanooda@ya.ru>
2020-11-10 23:43:19 +0200

    findMove

--- js/robot.js
+++ js/robot.js
@@ -1,3 +1,4 @@
+// TODO: clone
 // Zobrist hashing
 
 import { Game } from './game.js';
@@ -17,9 +18,23 @@ function evaluate(game, player) { // -100 <= result <= 100
 }
 function findMove(game) {
     const robotPlayer = game.getCurrentPlayer();
-    const moves = game.getMoves();
+    let moves = game.getMoves(); // movesWithE
+    for (const move of moves) {
+        let _game = new Game();
+        _game.items = JSON.parse(JSON.stringify(game.items));
+        _game.pairs = JSON.parse(JSON.stringify(game.pairs));
+        _game.history = JSON.parse(JSON.stringify(game.history));
+        _game.winner = game.winner;
 
-    return getRandomMove(game);
+        _game.move([move[0], move[1]], move[2]);
+        const _e = evaluate(_game, robotPlayer);
+        move.push(_e);
+    }
+    (a, b) => a[diagonal] - b[diagonal] || a[nextDiagonal] - b[nextDiagonal]
+    moves.sort((a, b) => a[3] - b[3]);
+    console.log(moves[moves.length - 1]);
+
+    return moves[moves.length - 1].slice(0, -1);
 }
 function getRandomMove(game) {
     const moves = game.getMoves();
