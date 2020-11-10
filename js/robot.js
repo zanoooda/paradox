8be6f9e60f64a54fdd9bf3000f31ae8f445b2b12
zanooda <zanooda@ya.ru>
2020-11-10 21:04:46 +0200

    init evaluate

--- js/robot.js
+++ js/robot.js
@@ -1,8 +1,11 @@
-// TODO: Evaluation: (game) => -100|100
-// Zobrist hashing for game state comparsing
+// Zobrist hashing
 
 import { Game } from './game.js';
 
+function evaluate(game) {
+    // ...
+    return 0; // -100 <= result <= 100
+}
 function findMove(game) {
     const allMoves = game.getAllMoves();
     const randomMove = allMoves[Math.floor(Math.random() * allMoves.length)];
